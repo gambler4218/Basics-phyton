b24a36dece728459564a073c56d7f7b6eefbe520
Konstantin <gambler4218@gmail.com>
2021-04-14 19:15:31 +0300

    Zadanie_1

---
+++ zadanie_1.py
@@ -0,0 +1,11 @@
+a = 10
+b = 38
+print(a)
+print(b)
+name = input('Введите Ваше имя: ')
+city = input('Введите город в котором живете: ')
+age = int(input('Сколько Вам лет: '))
+print(name)
+print(city)
+print(age)
+print(f'{name}, Вы проживаете в городе {city}, Ваш возраст {age}')
