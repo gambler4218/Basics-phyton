96a5629ebcda0c38e68ed5cbd34697431add71ba
Konstantin <gambler4218@gmail.com>
2021-04-22 20:08:38 +0300

    zadanie

---
+++ zadanie_1.py
@@ -0,0 +1,6 @@
+spisok = [2, 4.5, 'black', ['f', 't', 'b'], ('n', 'i', 'r'), {400, 'dog', None}, {1: 'Январь', 2: 'Февраль', 3: 'Март'}, True, None]
+i = 0
+
+while i < int(len(spisok)):
+    print(type(spisok[i]))
+    i +=1
